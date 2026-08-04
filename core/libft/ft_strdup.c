45c002072d53f23b847d4bd934bd53fefb3e2f9d
wslim <imeva.ws@gmail.com>
2026-08-04 17:44:49 +0800

    makefile

--- core/libft/ft_strdup.c
+++ core/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 16:54:00 by werlim            #+#    #+#             */
-/*   Updated: 2026/08/04 17:12:13 by werlim           ###   ########.fr       */
+/*   Updated: 2026/08/04 17:42:10 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,11 @@ char	*ft_strdup(const char *s1)
 {
 	char		*ptr;
 	size_t		strlength;
-	size_t		i;
 
 	strlength = ft_strlen(s1);
 	ptr = (char *)malloc(sizeof(char) * (strlength + 1));
 	if (ptr == NULL)
 		return (NULL);
-	i = 0;
 	ft_strlcpy(ptr, s1, strlength + 1);
 	return (ptr);
 }
