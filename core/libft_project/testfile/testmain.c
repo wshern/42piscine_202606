db0d307dda7bc2cb1ee00200e71825ba1dfe1a76
wslim <imeva.ws@gmail.com>
2026-08-04 22:41:29 +0800

    testfile for libft

--- core/libft_project/testfile/testmain.c
+++ core/libft_project/testfile/testmain.c
@@ -12,17 +12,17 @@ static void test_strlen(void)
 
 	if (ft_strlen(a) != strlen(a))
 	{
-		printf("ft_strlen error - short\n");
+		printf("ft_strlen FAIL - short\n");
 		return;
 	}
 	else if (ft_strlen(b) != strlen(b))
 	{
-		printf("ft_strlen error - empty\n");
+		printf("ft_strlen FAIL - empty\n");
 		return;
 	}
 	else if (ft_strlen(c) != strlen(c))
 	{
-		printf("ft_strlen error - long\n");
+		printf("ft_strlen FAIL - long\n");
 		return;
 	}
 	else
@@ -178,6 +178,111 @@ static void test_memcpy(void)
 		printf("ft_memcpy OK!\n");
 }
 
+static void test_memmove(void)
+{
+	char ctrl_src[] = "Test string";
+	char ctrl_dest[12];
+	char test_src[] = "Test string";
+	char test_dest[12];
+
+	memmove(ctrl_dest, ctrl_src, 5);
+	ft_memmove(test_dest, test_src, 5);
+	if (memcmp(ctrl_dest, test_dest, 5) != 0)
+	{
+		printf("ft_memmove FAIL - content\n");
+		return;
+	}
+
+	char ctrl[] = "Test string";
+	char test[] = "Test string";
+
+	memmove(ctrl + 2, ctrl, 5);
+	ft_memmove(test + 2, test, 5);
+	if (memcmp(ctrl, test, 12) != 0)
+	{
+		printf("ft_memmove FAIL - overlap (dest > src)!\n");
+		return ;
+	}
+	char ctrl2[] = "Test string";
+	char test2[] = "Test string";
+
+	memmove(ctrl2, ctrl2 + 2, 5);
+	ft_memmove(test2, test2 + 2, 5);
+	if (memcmp(ctrl2, test2, 12) != 0)
+		printf("ft_memmove FAIL - overlap (dest < src)!\n");
+	else
+		printf("ft_memmove OK!\n");
+}
+
+static void test_memchr(void)
+{
+	char s[] = "Hello world";
+
+	if (memchr(s, 'w', 12) != ft_memchr(s, 'w', 12))
+		printf("ft_memchr FAIL - found error!\n");
+	else if (memchr(s, 'z', 12) != ft_memchr(s, 'z', 12))
+		printf("ft_memchr FAIL - not found error!\n");
+	else if (memchr(s, 0, 12) != ft_memchr(s, 0, 12))
+		printf("ft_memchr FAIL - null byte should be found!\n");
+	else if (memchr(s, 'w', 3) != ft_memchr(s, 'w', 3))
+		printf("ft_memchr FAIL - n too small!\n");
+	else if (memchr(s, 200, 12) != ft_memchr(s, 200, 12))
+		printf("ft_memchr FAIL - byte > 127!\n");
+	else
+		printf("ft_memchr OK!\n");
+}
+
+// for memcmp and strncmp
+int sign(int n)
+{
+	if (n > 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (0);
+}
+
+static void test_memcmp(void)
+{
+	char *s1 = "Test 12345";
+	char *s2 = "Test 12a45";
+	char *a = "\200";
+	char *b = "\0";
+
+	if (sign(memcmp(s1, s2, 11)) != sign(ft_memcmp(s1, s2, 11)))
+		printf("ft_memcmp FAIL - differ\n");
+	else if (sign(memcmp(s1, s1, 11)) != sign(ft_memcmp(s1, s1, 11)))
+		printf("ft_memcmp FAIL - identical\n");
+	else if (sign(memcmp(s1, s2, 0)) != sign(ft_memcmp(s1, s2, 0)))
+		printf("ft_memcmp FAIL - n=0\n");
+	else if (sign(memcmp(a, b, 1)) != sign(ft_memcmp(a, b, 1)))
+		printf("ft_memcmp FAIL - byte > 127\n");
+	else
+		printf("ft_memcmp OK\n");
+}
+
+static void test_strlcpy(void)
+{
+	char *src = "HelloWorld";
+	char dest1[11];
+	char dest2[11];
+
+	if (strlcpy(dest1, src, 11) != ft_strlcpy(dest2, src, 11))
+		printf("ft_strlcpy FAIL - result error!\n");
+	else if (memcmp(dest1, dest2, 11) != 0)
+		printf("ft_strlcpy FAIL - content error!\n");
+	else if (strlcpy(dest1, src, 7) != ft_strlcpy(dest2, src, 7))
+		printf("ft_strlcpy FAIL - size different!\n");
+	else if (memcmp(dest1, dest2, 7) != 0)
+		printf("ft_strlcpy FAIL - content different\n");
+	else if (strlcpy(dest1, src, 0) != ft_strlcpy(dest2, src, 0))
+		printf("ft_strlcpy FAIL - must write nothing!\n");
+	else if (strlcpy(dest1, src, 1) != ft_strlcpy(dest2, src, 1))
+		printf("ft_strlcpy FAIL - more than NULL error!\n");
+	else
+		printf("ft_strlcpy OK!\n");
+}
+
 int	main(void)
 {
 	test_strlen();
@@ -189,5 +294,9 @@ int	main(void)
 	test_memset();
 	test_bzero();
 	test_memcpy();
+	test_memmove();
+	test_memchr();
+	test_memcmp();
+	test_strlcpy();
 	return (0);
 }
