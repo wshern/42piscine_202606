7d648518d7a7e3c0e345d5d61947ae1dbe4f3e73
wslim <imeva.ws@gmail.com>
2026-08-06 21:56:15 +0800

    ft_strjoin & libft

--- core/libft_project/libft/libft.h
+++ core/libft_project/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 16:39:31 by werlim            #+#    #+#             */
-/*   Updated: 2026/08/04 17:13:37 by werlim           ###   ########.fr       */
+/*   Updated: 2026/08/06 20:43:03 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,7 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int			ft_atoi(const char *str);
 void		*ft_calloc(size_t count, size_t size);
 char		*ft_strdup(const char *s1);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
 
 #endif
