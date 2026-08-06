f1c398ad9fdf9f417ceecc6a5c2048571948fdb6
wslim <imeva.ws@gmail.com>
2026-08-06 16:01:45 +0800

    added check for overflow

--- core/libft_project/libft/ft_calloc.c
+++ core/libft_project/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: werlim <werlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 16:38:03 by werlim            #+#    #+#             */
-/*   Updated: 2026/08/04 16:53:39 by werlim           ###   ########.fr       */
+/*   Updated: 2026/08/06 15:56:45 by werlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count != 0 && (count * size) / count != size)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (ptr == NULL)
 		return (NULL);
 	ft_bzero(ptr, (count * size));
 	return (ptr);
 }
+
+// first check overflow, count cannot be zero because div 0 = error
+// then only malloc
